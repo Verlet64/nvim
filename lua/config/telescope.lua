3227e2dfe520d7059d802d123d880e96a244849a
Tawhid <t.hannan93@gmail.com>
2024-03-03 14:00:54 +0000

    Improve interactivity of Telescope by enabling file creation, key hints
    - which-key has been added to provide simpler registration
      of subcommands, with the added ability to provide shortcut
      hints whilst commands are input
    - telescope-file-browser displays file in a way that maps
      more closely to the underlying file system, rather than a
      list of all files (including those in subdirectories) under
      the current path
    - telescope-project allows the registration of directories
      as projects, which makes it easier to jump between directories
      of interest

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,6 +1,35 @@
-local telescope = require('telescope.builtin')
+local telescope = require('telescope')
+local project = require("telescope._extensions.project.actions")
 
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
+telescope.setup {
+	  pickers = {
+		  buffers = {
+			  initial_mode = 'normal'
+		  }
+	  },
+	  extensions = {
+	        project = {
+			on_project_selected = function(prompt_bufnr)
+				project.browse_project_files(prompt_bufnr)
+			end
+		}
+	}
+}
+
+telescope.load_extension('project')
+
+local wk = require('which-key')
+
+wk.register({
+	f = {
+		-- f = { "<cmd>Telescope find_files<cr>", "File Browser" },
+		f = { [[<cmd>Telescope file_browser<cr>]], "File Browser"},
+		p = { function () telescope.extensions.project.project{} end, "List Projects"},
+		n = { "Create File or Directory" },
+		r = { "<cmd>Telescope oldfiles<cr>", "List Recent Files" },
+		g = { "<cmd>Telescope live_grep<cr>", "Grep files" } ,
+		b = { "<cmd>Telescope buffers<cr>", "List buffers" },
+		h = { "<cmd>Telescope help_tags<cr>", "Show Telescope Help"},
+
+	}
+}, { prefix = "<leader>"} )
