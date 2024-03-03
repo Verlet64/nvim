d2d836e6c7223a8e303b890cc3e7876920934a0e
Tawhid <t.hannan93@gmail.com>
2024-03-03 14:05:49 +0000

    Create key mappings for stepping in/out/over when debugging, and key hints for setting breakpoints

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -45,8 +45,17 @@ dap.listeners.before.event_exited.dapui_config = function()
   dapui.close()
 end
 
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_in() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end)
+local wk = require("which-key")
+
+wk.register({
+	name = "debug",
+	d = {
+		s = {
+			name = "step",
+			k = { "<cmd>DapStepOut<cr>", "step out"},
+			j = { "<cmd>DapStepInto<cr>", "step into"},
+			l = { "<cmd>DapStepOver<cr>", "step over"},
+		},
+		b = { "<cmd>DapToggleBreakpoint<cr>", "toggle breakpoint" }
+	}
+}, { prefix = "<leader>" })
