73eee693806ba93a18639888f507750e31dc8234
Tawhid Hannan <t.hannan93@gmail.com>
2024-03-04 17:08:22 +0000

    Redefine Telescope commands, grouping functions for listing files

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -22,13 +22,13 @@ local wk = require('which-key')
 
 wk.register({
 	f = {
-		-- f = { "<cmd>Telescope find_files<cr>", "File Browser" },
-		f = { [[<cmd>Telescope file_browser<cr>]], "File Browser"},
+		f = { "<cmd>Telescope find_files<cr>", "File Browser" },
+		d = { [[<cmd>Telescope file_browser<cr>]], "File Browser"},
+		b = { "<cmd>Telescope buffers<cr>", "List buffers" },
+		r = { "<cmd>Telescope oldfiles<cr>", "List Recent Files" },
 		p = { function () telescope.extensions.project.project{} end, "List Projects"},
 		n = { "Create File or Directory" },
-		r = { "<cmd>Telescope oldfiles<cr>", "List Recent Files" },
 		g = { "<cmd>Telescope live_grep<cr>", "Grep files" } ,
-		b = { "<cmd>Telescope buffers<cr>", "List buffers" },
 		h = { "<cmd>Telescope help_tags<cr>", "Show Telescope Help"},
 
 	}
