c54080a784fcd7273c2743f39541539bab211c3e
Tawhid Hannan <t.hannan93@gmail.com>
2024-03-04 17:06:19 +0000

    Create a user defined command for installing all mason packages
    - This makes the initial startup experience easier as it's not
      required to run :MasonInstall for each individual package.
    - This isn't run automatically on startup as it risks slowing
      down the editor.

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -1,1 +1,21 @@
-require("mason").setup()
+local present, mason = pcall(require, "mason")
+
+if not present then
+  return
+end
+
+local options = {
+	ensure_installed = {
+		"delve",
+		"codelldb",
+		"gopls",
+		"lua-language-server",
+	}
+}
+
+vim.api.nvim_create_user_command("MasonInstallAll", function()
+	local i = options.ensure_installed
+	vim.cmd("MasonInstall " .. table.concat(i, " "))
+end, {})
+
+mason.setup(options)
